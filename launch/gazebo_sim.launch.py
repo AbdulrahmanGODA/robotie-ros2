b436ba1a6c21315780116f676ce20b8a49a14c7b
AbdulrahmanGODA <01149016121tra@gmail.com>
2024-07-13 16:07:00 +0300

    ackermann_steering

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
     ld.add_action(rsp_include)
     ld.add_action(gazebo_include)
     ld.add_action(spawn_entity)
-    #ld.add_action(launch_rviz2)
+    ld.add_action(launch_rviz2)
     ld.add_action(diff_drive_spawner)
     ld.add_action(joint_broad_spawner)
 
